34d770151b2f5ec7e83bb00ec6bfff66512253d0
turbosnail <denisbatya@yandex.ru>
2017-02-05 12:48:26 +0300

    WIP: work with layers

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-mouseX = 0
-mouseY = 0
-mapSize = 2560;
-zoom = 1;
+mouseX = 0;
+mouseY = 0;
 
-d1 = false;
-d2 = false;
-rects = new Array();
-points=new Array();
-polygons = new Array();
+// move circle
+drag = false;
+activeCircle = null;
+waypoints = [];
 
 $(function(){
     canvasDiv=document.getElementById("canvas");
     window.gr = new jxGraphics(canvasDiv);
 
-    $(document).on('mousemove','#canvas',getMouseXY);
-    $(document).on('click','#canvas',drawPoint);
+    $(document).on('mousemove','#canvas',function(e){
+        getMouseXY(e);
 
+        if (drag) {
+            if (activeCircle) {
+                activeCircle.center = new jxPoint(mouseX, mouseY);
+                activeCircle.draw(gr);
+                reDrawPolygon();
+            }
+        }
+    });
 
-
-    // Zoom
-
-    /*$(document.body).on('mousewheel',function(event){
-
-        zoom += event.deltaY/2;
-
-        zoom = zoom<1?1:zoom;
-        zoom = zoom>10?10:zoom;
-
-        // $('#canvas').css()
-        canvasDiv.style.width = canvasDiv.style.height = canvasDiv.style["background-size"] = mapSize * zoom + 'px';
-        gr.getSVG().style.width = gr.getSVG().style.height = mapSize * zoom + 'px';
-    });*/
-
-
+    $(document).on('mouseup','#canvas',function(){
+        drag = false;
+        if(activeCircle == null) {
+            createCirlce(true);
+        }
+        activeCircle = null;
+        reDrawPolygon();
+    });
 
     //---------------------------------------------------
 })
@@ -87,161 +84,109 @@ function getBrush()
     return new jxBrush( getColor() )
 }
 
-function checkPoints(noAlert)
+function createCirlce(show)
 {
-    if(!noAlert)
+    var layer = getLayer();
+
+    if(typeof layer != 'string' || layer.length == 0)
+        return;
+
+    var cir = new jxCircle(new jxPoint(mouseX,mouseY), 5, getPen(), getBrush());
+    cir.id = layers[ getLayer()].circles.length;
+
+    if(show)
+        cir.draw(gr);
+
+    cir.addEventListener('mousedown', circleMouseDown);
+    cir.addEventListener('mouseup', circleMouseUp);
+    cir.addEventListener('mouseover', circleMouseOver);
+    cir.addEventListener('mouseout', circleMouseOut);
+    layers[ getLayer()].circles.push( cir );
+    return cir;
+}
+
+//Mousedown event handler for circle
+function circleMouseDown(evt, obj) {
+
+    drag = true;
+    activeCircle = obj;
+
+}
+
+//Mouseup event handler for circle
+function circleMouseUp(evt, obj) {
+    //activeCircle = null;
+}
+
+function circleMouseOver(evt, obj) {
+
+    document.body.style.cursor = "pointer";
+
+    obj.brush = new jxBrush(new jxColor("red"));
+    obj.draw(gr);
+
+}
+
+function circleMouseOut(evt, obj) {
+
+    document.body.style.cursor = "inherit";
+
+    obj.brush = new jxBrush(getColor());
+    obj.draw(gr);
+
+}
+
+function reDrawPolygon() {
+
+    for(var i in layers)
     {
-        if(points.length==0)
+        if(layers.hasOwnProperty( i ))
         {
-            alert("Please click at any location on the blank canvas at left side to plot the points!");
-            return false;
+            if(layers[ i ].circles.length < 3)
+                continue;
+
+            if (typeof layers[ i ].polygon == 'undefined')
+                layers[ i ].polygon = new jxPolygon([], getPen(), getBrush())
+
+            var points = [];
+
+            for (var j in layers[ i ].circles) {
+                points.push( layers[ i ].circles[ j ].center )
+            }
+
+            layers[ i ].polygon.points = points;
+            layers[ i ].polygon.draw(gr);
         }
-        else if(points.length<3)
-        {
-            alert("3 or more points are required to draw a polygon! Please plot more points by clicking at any location on the blank canvas at left side.");
-            return false;
-        }
     }
-    return true;
 }
 
-function drawPoint()
-{
-
-    rect = new jxRect(new jxPoint(mouseX-1,mouseY-1),2,2,getPen(), getBrush());
-    rect.draw(gr);
-    rects.push(rect);
-    var point = new jxPoint(mouseX,mouseY)
-    points.push(point);
-    return point;
-}
-
-
-function drawPolyline()
-{
-    if(!checkPoints())
-        return;
-
-    polyline = new jxPolyline(points,getPen(), getBrush());
-    polyline.draw(gr);
-
-    ShowPoints();
-}
-
-function drawLine()
-{
-    if(!checkPoints())
-        return;
-
-    gr.drawLine(pen,points[points.length-2],points[points.length-1]);
-
-    ShowPoints();
-}
-
-function fillPolygon()
-{
-    if(!checkPoints())
-        return;
-
-    polygon = new jxPolygon(points,getPen(), getBrush());
-    polygon.draw(gr);
-
-    polygons[polygons.length] = polygon;
-
-    ShowPoints();
-    clearPreviousPoints();
-}
-
-
-function clearCanvas()
-{
-    //gr.clear();
-
-    for(i in polygons)
-    {
-        polygons[i].remove()
-    }
-
-    for(i in rects)
-    {
-        rects[i].remove()
-    }
-
-
-    points=new Array();
-    polygons = new Array();
-    rects = new Array();
-    var txt=document.getElementById("txt").value = "";
-}
-
-function clearPreviousPoints()
-{
-    for(i in rects)
-    {
-        rects[i].remove()
-    }
-
-    points=new Array();
-    rects = new Array();
-}
-
-function ShowPoints()
-{
-    var txt = document.getElementById("txt");
-    var format = document.getElementById("output").value;
-    // txt.value="";
-    switch (format) {
-            case 'json':
-                if(txt.value.length)
-                    txt.value += ',\n';
-
-                txt.value += '{\n  X:[';
-                for(var i=0;i<points.length;i++)
-                {
-                    if(i>0)
-                        txt.value += ', ';
-
-                    txt.value +=  (points[i].x - 1280);
-                }
-                txt.value += '],';
-
-                txt.value += '\n  Y:[';
-                for(var i=0;i<points.length;i++)
-                {
-                    if(i>0)
-                        txt.value += ', ';
-
-                    txt.value +=  (1280 - points[i].y);
-                }
-                txt.value += '],\n}';
-            break;
-            case 'php':
-                if(txt.value.length)
-                    txt.value += '\n';
-
-                txt.value += "array(";
-                for(var i=0;i<points.length;i++)
-                {
-                    if(i>0)
-                        txt.value += ',';
-
-                    txt.value += '\n    array("x" => ' + (points[i].x - 1280) + ', "y" => ' + (1280 - points[i].y) + ")";
-                }
-
-                txt.value += '\n)'
-            break;
-            default:
-                for(var i=0;i<points.length;i++)
-                {
-                    txt.value +=  (points[i].x - 1280) + ";" + (1280 - points[i].y) + "\n";
-                }
-                break;
-    }
-
-}
 
 function track(track)
 {
     canvasDiv.style.backgroundImage = "url(http://img.lfs.net/remote/maps/"+track+".jpg)";
     return false;
+}
+
+window.layers = {};
+
+function addLayer() {
+    var op = document.createElement('option');
+    var name = prompt('Input layer name');
+    if(name.length <= 0){
+        alert('name is empty');
+        return undefined;
+    }
+    op.selected = true;
+    op.value = op.innerHTML = name;
+
+   $('#layer').append(op);
+
+    layers[name] = {};
+
+    layers[name].circles = [];
+    layers[name].speedLimit = 0;
+}
+
+function getLayer(){
+    return $('#layer').val();
 }
